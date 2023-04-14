2d9c9172a249a337417ddb194ad33f9c97aa05f7
MaxKuznets0v <max.kuznetsov@yahoo.com>
2023-04-15 00:19:09 +0400

    added localisation for piece of art categories

--- Controllers/FeedController.cs
+++ Controllers/FeedController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Localization;
 using System.Security.Claims;
 
 namespace BlogReview.Controllers
@@ -13,10 +14,13 @@ namespace BlogReview.Controllers
     {
         private readonly ArticleContext articleContext;
         private readonly UserManager<User> userManager;
-        public FeedController(ArticleContext context, UserManager<User> userManager) 
+        private readonly IStringLocalizer<FeedController> localizer;
+        public FeedController(ArticleContext context, UserManager<User> userManager, 
+            IStringLocalizer<FeedController> localizer) 
         { 
             articleContext = context;
             this.userManager = userManager;
+            this.localizer = localizer;
         }
         public IActionResult Index()
         {
@@ -25,6 +29,7 @@ namespace BlogReview.Controllers
         [Authorize]
         public IActionResult CreateArticle(Guid id)
         {
+            SetGroupsToViewData();
             if (id != Guid.Empty)
             {
                 Article existingEntry = articleContext.Articles.FirstOrDefault(x => x.Id == id);
@@ -74,6 +79,17 @@ namespace BlogReview.Controllers
         private async Task<User> GetUser(string userId)
         {
             return await userManager.FindByIdAsync(userId);
+        }
+        private void SetGroupsToViewData()
+        {
+            ViewData["Groups"] = new List<string> 
+            { 
+                localizer["ArticleObjectGroupBooks"],
+                localizer["ArticleObjectGroupGames"],
+                localizer["ArticleObjectGroupMovies"],
+                localizer["ArticleObjectGroupTVseries"],
+                localizer["ArticleObjectGroupOthers"]
+            };
         }
     }
 }
