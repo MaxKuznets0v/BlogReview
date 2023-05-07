b6be6b11afcb8626679c054dc500a140c516a5d5
MaxKuznets0v <max.kuznetsov@yahoo.com>
2023-05-07 14:24:21 +0400

    added request filter in case of user was blocked or removed

--- Controllers/ArticleController.cs
+++ Controllers/ArticleController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using BlogReview.Services;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace BlogReview.Controllers
 {
@@ -16,6 +17,19 @@ namespace BlogReview.Controllers
         {
             this.userService = userService;
             this.articleStorage = articleStorage;
+        }
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            Task<User?> userTask = userService.GetUser(User);
+            userTask.Wait();
+            if (User.Identity != null && User.Identity.IsAuthenticated &&
+                context.ActionDescriptor.RouteValues["action"] != "Logout" &&
+                (userTask.Result == null || userService.IsUserBlocked(userTask.Result)))
+            {
+                context.Result = RedirectToAction("Logout", "Account");
+                return;
+            }
+            base.OnActionExecuting(context);
         }
     }
 }
